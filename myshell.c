b14559b7297b03c571615ebc9a13a64058c05fa0
abd432018 <abdullah.eid@stu.fsm.edu.tr>
2021-12-06 07:28:19 +0300

    modify_myShell

--- myshell.c
+++ myshell.c
@@ -1,6 +1,64 @@
-#include <stdio.h>
+v#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
+
+void copyArray(char*, char*, int);
+int control(char**, int,char**);
+int length(char*);
+int charCount(char*, char);
+void printWords(char**, int);
+char** split(char*,char*);
+int hasOr(char*, char);
+
+
+int main(int argc, char *argv[], char** envp){
+    int bexit;
+    char* newargv[3];
+    char str[50];
+    char temp[50];
+     printf("myshell>> ");
+    while(scanf("%[^\n]",str)==1){ 
+        copyArray(str, temp,50);     
+        short wordCount=charCount(str, ' ')+1;
+        short orCount=charCount(str, '|');
+        char **words=split(str, " ");
+        //printf("word count: %d\n",orCount);      
+        //printWords(words, wordCount);
+        if(hasOr(temp,'|')){
+            bexit=control(words, wordCount, envp);   
+            int i,x=0;
+            for(i=0;i<wordCount;i++){
+                if(strcmp("|",*words) == 0){
+                    words++;
+                    bexit=control(words, wordCount-(i+1), envp);
+                    x++;
+                    if(orCount==x || bexit)break;      
+                }else{ 
+                    words++;
+                }
+            }  
+        }else{
+            bexit=control(words, wordCount, envp);
+        }
+        if(bexit){
+           break;        
+        }
+        printf("myshell>> ");
+        scanf("%[^\n]",str);
+        getchar();
+    }
+    
+    return 0;
+
+}
+
+
+/**********************************functions**********************************/
+/**********************************functions**********************************/
+/**********************************functions**********************************/
+/**********************************functions**********************************/
+
 int length(char* str){
     int counter = 0;
     while(*str != '\0'){
@@ -20,54 +78,65 @@ int charCount(char* str,char c){
     }
     return counter;
 }
-
 void printWords(char** words, int size){
     int i;
     for(i=0;i<size;i++){
         printf("words[%d] = %s\n", i, words[i]);
     }
 }
-char** split(char* str){
-    short wordCount=charCount(str, ' ')+1;
+char** split(char* str,char* c){
+    short wordCount=charCount(str, c)+1;
     char **words = (char**) malloc(wordCount* sizeof(char *));
-    char *token = strtok(str, " ");// Returns first token
+    char *token = strtok(str, c);// Returns first token
     // Keep printing tokens while one of the
     // delimiters present in str[].
     int counter =0;
     while (token != NULL){   
         *(words+counter++)=token;
-        token = strtok(NULL, " ");
+        token = strtok(NULL, c);
     }
     return words;
 }
-void tekrar(char** words){
-    int i;
-    int tekrarSayisi = atoi(*(words+2));
-    for(i=0;i<tekrarSayisi;i++){
-        printf("%s\n",*(words+1));
+int hasOr(char* str, char c){
+    int i,len;
+    len = length(str);
+    for(i=0;i<len;i++){
+        if(*(str+i) == c){
+            return 1;
+        }
     }
-    
+    return 0;
 }
-int main(int argc, char *argv[], char** envp){
-    char* newargv[3];
-    char str[50];
-     printf("myshell>> ");
-    while(scanf("%[^\n]",str)==1){      
-        short wordCount=charCount(str, ' ')+1;
-        char **words=split(str);
-        printf("word count: %d\n",wordCount);      
-        printWords(words, wordCount);
-        if(strcmp("exit",*(words)) == 0){
-            break;
-        }else if(strcmp("tekrar",*(words)) == 0){
+void copyArray(char* str1, char* str2, int size){
+    int i;
+    for(i=0;i<size;i++){
+        str2[i]=str1[i];
+    }
+}
+int control(char** words, int wordCount, char** envp){
+    char* newargv[4];
+    if(strcmp("exit",*(words)) == 0){
+            return 1;
+        }else if(strcmp("clear",*(words)) == 0){
+            system("clear");
+        }else if(strcmp("ls",*(words)) == 0){
+            system("ls");
+        }else if(strcmp("cat",*(words)) == 0){
             int i;
-            int k, h, status;
+            printf("cat: ");
+            for(i=1;i<wordCount;i++){
+                printf("%s ", words[i]);
+            }
+            printf("\n");
+        }else if(strcmp("tekrar",*(words)) == 0){
+            int i, pid;
 
-            k = fork();
-
-            if (k > 0) {
-                h = wait(&status);
-            } else {
+            pid = fork();
+            if (pid > 0) {
+                wait(&i);
+            } 
+            else{
+            //if (pid == 0) {
                 newargv[0]=*(words+2);
                 newargv[1]=*(words+1);
                 newargv[2]=NULL;
@@ -75,21 +144,43 @@ int main(int argc, char *argv[], char** envp){
                 perror("error\n");
                 exit(1);
             }
-            //int tekrarSayisi = atoi(*(words+2));
-            //for(i=0;i<tekrarSayisi;i++){
-            //    printf("%s\n",*(words+1));
-            //}
+        }else if(strcmp("bash",*(words)) == 0){
+            //int i;
+            int pid, h, status;
+
+            pid = fork();
+
+            if (pid > 0) {
+                wait(&status);
+            } else {
+                newargv[0]="bash>>";
+                newargv[1]=NULL;
+                status=execve("/bin/bash", newargv, envp);
+                perror("error\n");
+                exit(1);
+            }
+        }else if(strcmp("islem",*(words)) == 0){
+            //int i;
+            int pid, status;
+
+            pid = fork();
+
+            if (pid > 0) {
+                wait(&status);
+            } else {
+                newargv[0]=*(words+1);
+                newargv[1]=*(words+2);
+                newargv[2]=*(words+3);
+                newargv[3]=NULL;
+                status=execve("islem", newargv, envp);
+                perror("error\n");
+                exit(1);
+            }
         }else{
             printf("yanlis bir komut girdiniz\n");
         }
-        printf("myshell>> ");
-        scanf("%[^\n]",str);
-        getchar();
-        
-        
-    }
-    
-    return 0;
+        return 0;
 
 }
+
 
